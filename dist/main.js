63ba652a3b810ffd5066b06d181002e4779b6d26
정훈박 <fireking5997@gmail.com>
2022-09-01 12:26:27 +0900

    fix:dist

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,24 @@ const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
+    const allowList = [
+        'http://fireking5997.com',
+        'http://www.fireking5997.com',
+        'http://localhost:3000',
+        process.env.BASE_URL,
+        `${process.env.BASE_URL}:3000`,
+    ];
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: (origin, callback) => {
+            const allowed = allowList.includes(origin);
+            if (allowed)
+                callback(null, true);
+            else
+                throw new common_1.UnauthorizedException('UnAccepted Cors');
+        },
+        credentials: true,
+    });
     app.setGlobalPrefix('api');
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
@@ -12,7 +29,6 @@ async function bootstrap() {
         transform: true,
         transformOptions: { enableImplicitConversion: true },
     }));
-    app.enableCors();
     await app.listen(process.env.PORT);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
-import { ValidationPipe } from '@nestjs/common';
+import { UnauthorizedException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const allowList = [
+    'http://fireking5997.com',
+    'http://www.fireking5997.com',
+    'http://localhost:3000',
+    process.env.BASE_URL,
+    `${process.env.BASE_URL}:3000`,
+  ];
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowed = allowList.includes(origin);
+      if (allowed) callback(null, true);
+      else throw new UnauthorizedException('UnAccepted Cors');
+    },
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,7 +30,6 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
-  app.enableCors();
   await app.listen(process.env.PORT);
 }
 bootstrap();
